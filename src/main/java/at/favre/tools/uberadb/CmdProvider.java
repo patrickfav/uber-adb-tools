2a788a313b75f58fe4b1655cd5a168788c62481a
pfavre <patrick.favrebulle@gmail.com>
2017-10-04 20:49:42 +0200

    Update copyright

--- src/main/java/at/favre/tools/uberadb/CmdProvider.java
+++ src/main/java/at/favre/tools/uberadb/CmdProvider.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  *  Copyright 2016 Patrick Favre-Bulle
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *
+ */
+
 package at.favre.tools.uberadb;
 
 import at.favre.tools.uberadb.util.CmdUtil;
@@ -62,11 +80,7 @@ public interface CmdProvider {
             Result result = runCmd(cmd);
             history.add(result);
 
-            if (result.exception != null) {
-                return false;
-            } else {
-                return true;
-            }
+            return result.exception == null;
         }
 
         @Override
